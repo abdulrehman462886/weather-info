60b09572e5e4d4d9f2352eb0c059bd3447a79fb2
mst intern 6 <mstintern6@gmail.com>
2023-01-09 18:01:35 +0500

    First

---
+++ src/Table.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./App.css";
+
+export default function Table(props) {
+  return (
+    <div className="container">
+      <div className="container">
+        <table className="table my-3">
+          <thead>
+            <tr>
+              <th scope="col">Date</th>
+              <th scope="col">Temp</th>
+              <th scope="col">Humidity</th>
+              <th scope="col">Pressure</th>
+              <th scope="col">WindSpeed</th>
+              <th scope="col">City</th>
+            </tr>
+          </thead>
+          <tbody>
+            {props.weatherData.list.map((items, index) => {
+              for (let i = 0; i < 3; i++) {
+                if (items.dt_txt.substring(8, 10) == new Date().getDate() + i)
+                  return ( 
+                    <tr key={index}>
+                      <td>{items.dt_txt}</td>
+                      <td>{Math.round(items.main.temp)}F</td>
+                      <td>{items.main.humidity}</td>
+                      <td>{items.main.pressure}</td>
+                      <td>{items.wind.speed}</td>
+                      <td>{props.weatherData.city.name}</td>
+                    </tr>
+                  );
+              }
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
